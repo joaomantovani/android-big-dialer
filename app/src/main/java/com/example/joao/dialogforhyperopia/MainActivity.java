e9e18fab9a7db9b9acfb214d46a4bdd7ab903b73
Joao Mantovani <jpm.mantovani45@gmail.com>
2018-06-26 00:00:33 -0300

    Buttons and caller textView working

--- app/src/main/java/com/example/joao/dialogforhyperopia/MainActivity.java
+++ app/src/main/java/com/example/joao/dialogforhyperopia/MainActivity.java
@@ -2,12 +2,71 @@ package com.example.joao.dialogforhyperopia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView mNumberCallTextView;
+    private static String NULL_START = "";
+    private NumberFormatter numberFormatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_0).setOnClickListener(this);
+        findViewById(R.id.btn_1).setOnClickListener(this);
+        findViewById(R.id.btn_2).setOnClickListener(this);
+        findViewById(R.id.btn_3).setOnClickListener(this);
+        findViewById(R.id.btn_4).setOnClickListener(this);
+        findViewById(R.id.btn_5).setOnClickListener(this);
+        findViewById(R.id.btn_6).setOnClickListener(this);
+        findViewById(R.id.btn_7).setOnClickListener(this);
+        findViewById(R.id.btn_8).setOnClickListener(this);
+        findViewById(R.id.btn_9).setOnClickListener(this);
+        findViewById(R.id.btn_0).setOnClickListener(this);
+
+        findViewById(R.id.btn_delete_once).setOnClickListener(this);
+        findViewById(R.id.btn_delete_all).setOnClickListener(this);
+
+        findViewById(R.id.btn_call).setOnClickListener(this);
+
+        mNumberCallTextView = findViewById(R.id.tv_number_call);
+        mNumberCallTextView.setText(NULL_START);
+
+        numberFormatter = new NumberFormatter(NULL_START);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String mNumber = mNumberCallTextView.getText().toString();
+
+        switch (v.getId()) {
+            case R.id.btn_delete_all:
+                // Reset the string to ""
+                mNumberCallTextView.setText(NULL_START);
+                break;
+            case R.id.btn_delete_once:
+                mNumberCallTextView.setText(
+                        mNumber.substring(0, mNumber.length() - 1)
+                );
+                break;
+            case R.id.btn_call:
+                // TODO: 6/25/18 Call the activity to phone call
+                Toast.makeText(this, "Call", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                // get the button clicked
+                Button button = (Button) v;
+
+                // Get the text from the button clicked
+                String number = button.getText().toString();
+                mNumberCallTextView.append(number);
+
+                break;
+        }
     }
 }
