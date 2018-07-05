ba29dcec091ab41f42f0a0c91fefb35c862bfae7
João Pedro Mantovani <jpm.mantovani45@gmail.com>
2018-07-05 00:39:28 -0300

    Font size improvement

--- app/src/main/java/com/example/joao/dialogforhyperopia/MainActivity.java
+++ app/src/main/java/com/example/joao/dialogforhyperopia/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Vibrator;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
